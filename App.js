fcb0c808d459790370c278b8687f645e47ff3c91
Abdelrahman Mohammed <>
2023-12-25 17:15:51 +0200

    add navigation to root

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import SplashScreen from "./src/screens/SplashScreen";
+import Profile from "./src/screens/Profile";
+import Onboarding from "./src/screens/Onboarding";
+import Home from "./src/screens/Home";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const [userData, setUserData] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@example.com",
+    phoneNumber: "1234567890",
+    avatarImage: null,
+    notifications: {
+      email: false,
+      passwordChanges: false,
+      specialOffers: false,
+      newsletter: false,
+    },
+  });
+
+  const [isOnboardingComplete, setIsOnboardingComplete] = useState(null);
+
+  useEffect(() => {
+    checkUserState();
+  }, []);
+
+  const checkUserState = async () => {
+    try {
+      const userState = await AsyncStorage.getItem("isOnboardingCompleted");
+      setIsOnboardingComplete(userState === "true" ? true : false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        {/* {isOnboardingComplete === null ? (
+          <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <>
+            {isOnboardingComplete ? (
+              <>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Profile" component={Profile} />
+              </>
+            ) : (
+              <Stack.Screen
+                name="Onboarding"
+                component={Onboarding}
+                options={{ headerShown: false }}
+              />
+            )}
+          </>
+        )} */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
